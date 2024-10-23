45a2bea29c4ae1427aca2cb8705185cc1aa52425
2bys <tobias.weber.hh@gmail.com>
2024-10-23 23:41:37 +0200

    [UPD] Removed flattening from `CrossEntropyLoss_criterion`.

--- kfs/basics/reduction_factors.py
+++ kfs/basics/reduction_factors.py
@@ -5,6 +5,7 @@ from typing import Union
 
 from torch import (
     Tensor,
+    gather,
     manual_seed,
     rand,
     rand_like,
@@ -83,19 +84,12 @@ def CrossEntropyLoss_criterion(
     Returns:
         The evaluated criterion.
     """
-    # TODO Figure out if one can do this without flattening
     log_softmax = prediction.log_softmax(dim=0)
-    log_softmax_flat = (
-        log_softmax.unsqueeze(-1)
-        if log_softmax.ndim == 1
-        else log_softmax.flatten(start_dim=1)
+    log_probs = gather(
+        log_softmax, dim=0, index=target.unsqueeze(0)
     )
-    target_flat = target.flatten()
-    return -sum(
-        log_softmax_flat[y_i, i]
-        for i, y_i in enumerate(target_flat)
-    )
-
+    return -log_probs.sum()
+    
 
 if __name__ == "__main__":
     manual_seed(0)
