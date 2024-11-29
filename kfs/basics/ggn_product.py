5f86a8fd822b669956b5e4a05ed6c2100559ea18
Balint Mucsanyi <mucsanyibalint99@gmail.com>
2024-11-29 17:34:00 +0100

    [FIX] Replace g by h to stay consistent with the text

--- kfs/basics/ggn_product.py
+++ kfs/basics/ggn_product.py
@@ -11,11 +11,11 @@ from kfs.basics.jacobian_products import jvp, vjp
 def ggnvp(
     f: Tensor,
     x: Union[Tensor, Tuple[Tensor, Tensor]],
-    g: Tensor,
+    h: Tensor,
     v: Tensor,
     retain_graph: bool = False,
 ) -> Tensor:
-    r"""Multiply v with the GGN of f linearized at g w.r.t. x.
+    r"""Multiply v with the GGN of f linearized at h w.r.t. x.
 
     The GGN corresponds to the Hessian of a function that
     uses a linearization at linearize w.r.t. x.
@@ -26,7 +26,7 @@ def ggnvp(
         f: The function whose GGN is multiplied with.
         x: The input of the function. If x is a tuple, the
             mixed GGN is multiplied onto v.
-        g: The output of the sub-function at which
+        h: The output of the sub-function at which
             the dependency on x is linearized.
         v: The vector that is multiplied with the GGN.
             Has same shape as x (or x[1] if it is a tuple).
@@ -39,7 +39,7 @@ def ggnvp(
         same shape as x[1].
     """
     x1, x2 = (x, x) if isinstance(x, Tensor) else x
-    Jv = jvp(g, x2, v, retain_graph=retain_graph)
-    HJ_v = hvp(f, g, Jv, retain_graph=retain_graph)
-    JTHJ_v = vjp(g, x1, HJ_v, retain_graph=retain_graph)
+    Jv = jvp(h, x2, v, retain_graph=retain_graph)
+    HJ_v = hvp(f, h, Jv, retain_graph=retain_graph)
+    JTHJ_v = vjp(h, x1, HJ_v, retain_graph=retain_graph)
     return JTHJ_v
