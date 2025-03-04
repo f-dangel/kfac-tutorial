0137a6a201c4af2a1def93335bb8c74612fbd17f
Bálint Mucsányi <50999947+bmucsanyi@users.noreply.github.com>
2025-03-04 23:29:37 +0100

    [ADD] MC Fisher product and tensor

---
+++ kfs/basics/mc_fisher_product.py
@@ -0,0 +1,110 @@
+"""MC-approximated Type-I Fisher-vector multiplication."""
+
+
+from torch import Generator, Tensor, zeros_like
+from torch.autograd import grad
+from torch.nn import CrossEntropyLoss, Module, MSELoss
+
+from typing import Union, Tuple
+
+from kfs.basics.label_sampling import (
+    draw_label_CrossEntropyLoss,
+    draw_label_MSELoss,
+)
+from kfs.basics.reduction_factors import (
+    CrossEntropyLoss_criterion,
+    MSELoss_criterion,
+    get_reduction_factor,
+)
+
+
+def mcfishervp(
+    model: Module,
+    loss_func: Module,
+    inputs: Tensor,
+    labels: Tensor,
+    params: Union[Tensor, Tuple[Tensor, Tensor]],
+    v: Tensor,
+    mc_samples: int,
+    seed: int = 42,
+    retain_graph: bool = False,
+) -> Tensor:
+    r"""Multiply v with the MC Fisher of model.
+
+    The MC (Type-I) Fisher corresponds to a Monte Carlo
+    approximation of the Fisher Information Matrix of
+    model w.r.t. loss_func using mc_samples samples.
+
+    See $\text{\Cref{sec:fisher}}$.
+
+    Args:
+        model: The model whose Fisher is approximated.
+        loss_func: The (scaled) negative log-likelihood
+            function.
+        inputs: The inputs over which the Fisher is
+            calculated.
+        labels: The labels corresponding to inputs.
+        params: The parameters of model w.r.t. which
+            we are calculating the Fisher.
+        v: The vector that is multiplied with the MC
+            Fisher. Has same shape as params.
+        mc_samples: The number of MC samples used for
+            the approximation.
+        seed: Optional seed for reproducibility.
+            Default: 42.
+        retain_graph: Whether to retain the computation
+            graph of f for future differentiation.
+            Default: False.
+
+    Returns:
+        The product of the Monte Carlo approximated Fisher
+        with v. Has same shape as params if params is a
+        Tensor, and params[0] if params is a tuple.
+    """
+    params = (
+        (params, params)
+        if isinstance(params, Tensor)
+        else params
+    )
+
+    generator = Generator()
+    generator.manual_seed(seed)
+
+    result = zeros_like(params[0])
+
+    if isinstance(loss_func, MSELoss):
+        c_func = MSELoss_criterion
+        sample_label_func = draw_label_MSELoss
+    elif isinstance(loss_func, CrossEntropyLoss):
+        c_func = CrossEntropyLoss_criterion
+        sample_label_func = draw_label_CrossEntropyLoss
+    else:
+        raise NotImplementedError(
+            f"Unknown loss function: {type(loss_func)}"
+        )
+
+    reduction_factor = get_reduction_factor(
+        loss_func, labels
+    )
+
+    for x in inputs.split(1):
+        pred_n = model(x)
+
+        for _ in range(mc_samples):
+            y_tilde_nm = sample_label_func(
+                pred_n, generator
+            )
+            c_nm = c_func(pred_n, y_tilde_nm)
+
+            g_nm1, g_nm2 = grad(
+                c_nm,
+                params,
+                retain_graph=retain_graph,
+            )
+            g_nm1, g_nm2 = g_nm1.detach(), g_nm2.detach()
+
+            result.add_(g_nm1 * (g_nm2 * v).sum())
+
+    result.mul_(reduction_factor / mc_samples)
+
+    return result
---
+++ kfs/basics/mc_fishers.py
@@ -0,0 +1,141 @@
+"""MC-approximated Type-I Fisher tensor and matrices."""
+
+from typing import Tuple, Union
+
+from torch import (
+    Tensor,
+    arange,
+    unravel_index,
+    zeros,
+    zeros_like,
+)
+from torch.nn import Module
+
+from kfs.basics.flattening import cvec, rvec
+from kfs.basics.mc_fisher_product import mcfishervp
+
+
+def mcfisher(
+    model: Module,
+    loss_func: Module,
+    inputs: Tensor,
+    labels: Tensor,
+    params: Union[Tensor, Tuple[Tensor, Tensor]],
+    mc_samples: int,
+    seed: int = 42,
+) -> Tensor:
+    r"""Compute the MC approximation of model's Fisher.
+
+    The MC (Type-I) Fisher corresponds to a Monte Carlo
+    approximation of the Fisher Information Matrix of
+    model w.r.t. loss_func using mc_samples samples.
+
+    See $\text{\Cref{def:mc_fisher}}$.
+
+    Args:
+        model: The model whose Fisher is approximated.
+        loss_func: The (scaled) negative log-likelihood
+            function.
+        inputs: The inputs over which the Fisher is
+            calculated.
+        labels: The labels corresponding to inputs.
+        params: The parameters of model w.r.t. which
+            we are calculating the Fisher.
+        mc_samples: The number of MC samples used for
+            the approximation.
+        seed: Optional seed for reproducibility.
+
+    Returns:
+        The Monte Carlo approximated Fisher. Has shape
+        params.shape + params.shape if
+        params is a Tensor, and
+        params[0].shape + params[1].shape if
+        params is a tuple.
+    """
+    params1, params2 = (
+        (params, params)
+        if isinstance(params, Tensor)
+        else params
+    )
+    (dtype,) = {params1.dtype, params2.dtype}
+    (device,) = {params1.device, params2.device}
+    F = zeros(
+        params1.shape + params2.shape,
+        dtype=dtype,
+        device=device,
+    )
+
+    for d in arange(params1.numel()):
+        d_unraveled = unravel_index(d, params1.shape)
+        one_hot_d = zeros_like(params1)
+        one_hot_d[d_unraveled] = 1.0
+        F[d_unraveled] = mcfishervp(
+            model,
+            loss_func,
+            inputs,
+            labels,
+            (params2, params1),
+            one_hot_d,
+            mc_samples,
+            seed,
+            retain_graph=True,
+        )  # mcfishervp is defined in $\text{\Cref{def:mcfishervp}}$
+
+    return F
+
+
+def vec_mcfisher(
+    model: Module,
+    loss_func: Module,
+    inputs: Tensor,
+    labels: Tensor,
+    params: Union[Tensor, Tuple[Tensor, Tensor]],
+    mc_samples: int,
+    vec: str,
+    seed: int = 42,
+) -> Tensor:
+    r"""Compute the vectorized MC Fisher matrix.
+
+    Args:
+        model: The model whose Fisher is approximated.
+        loss_func: The (scaled) negative log-likelihood
+            function.
+        inputs: The inputs over which the Fisher is
+            calculated.
+        labels: The labels corresponding to inputs.
+        params: The parameters of model w.r.t. which
+            we are calculating the Fisher.
+        mc_samples: The number of MC samples used for
+            the approximation.
+        vec: Name of the flattening scheme. Must be
+            either 'rvec' or 'cvec'.
+        seed: Optional seed for reproducibility.
+
+
+    Returns:
+        The rvec- or cvec-MC-Fisher matrix of model
+        w.r.t. loss_func.
+        Has shape (params.numel(), params.numel()) if
+        params is a Tensor, and
+        (params[0].numel(), params[1].numel()) if
+        params is a tuple.
+    """
+    vec = {"cvec": cvec, "rvec": rvec}[vec]
+    F = mcfisher(
+        model,
+        loss_func,
+        inputs,
+        labels,
+        params,
+        mc_samples,
+        seed,
+    )
+    # flatten row indices
+    row_ndim = (
+        params.ndim
+        if isinstance(params, Tensor)
+        else params[0].ndim
+    )
+    F = vec(F, end_dim=row_ndim - 1)
+    # flatten column indices
+    return vec(F, start_dim=1)
